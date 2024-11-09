b721fcc2787809284e8c79d122fd44e01f2df9b3
shankar22-cyber <83104729+shankar22-cyber@users.noreply.github.com>
2024-11-09 08:28:10 +0530

    Add files via upload

---
+++ result.py
@@ -0,0 +1,11 @@
+# my_dict ={"name" : "JOHN", "age" :25}
+# my_dict["name"] = "SHANKAR"
+# print(my_dict)
+def pattern(n):
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+
+            return "shankar"
+
+result = pattern(5)
+print(result)
